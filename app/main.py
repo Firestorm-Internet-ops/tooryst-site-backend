1870e2cc7729fd9bc3548e8c2b354b7a7ee4003d
Deepak <fansforever711@gmail.com>
2026-01-07 15:21:05 +0530

    added staging CORS allow list

--- app/main.py
+++ app/main.py
@@ -61,7 +61,7 @@ def create_app() -> FastAPI:
         allow_origins=[
             "https://tooryst.co",
             "https://www.tooryst.co",
-            "https://staging.tooryst.co"
+            "https://staging.tooryst.co",
             "http://localhost:3000",
             "http://localhost:3001",
         ],
